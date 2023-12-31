c01976a656a6cf79c921e54b14626a173fa271f8
Kartavya Sharma <kartavya@berkeley.edu>
2023-12-30 18:59:53 -0800

    Using Bun + added basic components

---
+++ src/components/Landing.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { HeadFC, PageProps } from 'gatsby';
+
+const Landing: React.FC = () => {
+    return (
+        <div>
+            Another hello.
+        </div>
+    )
+}
+
+export default Landing;
